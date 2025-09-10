07918b967b458bb71f429c40d17bc8d9671fe66e
Isaac Lee <32625683+isaacylee@users.noreply.github.com>
2025-09-10 16:11:46 -0700

    Add border on button to meet contrast threshold

--- src/pages/landings/evictions/index.jsx
+++ src/pages/landings/evictions/index.jsx
@@ -35,6 +35,7 @@ export default function Evictions() {
                 className='mb-4 rounded px-4 py-2 font-bold text-black'
                 style={{
                   backgroundColor: '#41ffca',
+                  border: '1px solid #2b9176'
                 }}
               >
                 Table Version
