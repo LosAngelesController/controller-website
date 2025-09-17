e988b9dce70867aab6e66ac553335651a56bb78b
Isaac Lee <32625683+isaacylee@users.noreply.github.com>
2025-09-17 11:49:23 -0700

    Add main landmark for keyboard nav

--- src/pages/landings/building-safety.jsx
+++ src/pages/landings/building-safety.jsx
@@ -42,7 +42,7 @@ export default function Employees() {
       <center>
         <Navbar />
       </center>
-      <main className='md:max-w container mx-auto px-4 sm:container lg:max-w-3xl xl:max-w-4xl'>
+      <main id="main-content" tabIndex={-1} className='md:max-w container mx-auto px-4 sm:container lg:max-w-3xl xl:max-w-4xl'>
         <center>
           <div className='mx-2 flex w-full flex-col px-4 py-2 sm:mx-4 md:px-0 lg:mx-auto lg:max-w-3xl xl:max-w-4xl'>
             <h1
