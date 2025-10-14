a4ba59b79d5885499ce852c53aba66f3e3540be2
na0nie <naonchun@gmail.com>
2025-10-14 15:33:21 -0700

    hide meu

--- src/pages/meu/index.tsx
+++ src/pages/meu/index.tsx
@@ -1,19 +1,19 @@
-import Hero from '../../components/meu/Hero';
-import KeyFindings from '../../components/meu/KeyFindings';
-import Recommendations from '../../components/meu/Recommendations';
-import Navbar from '../../components/Navbar';
+// import Hero from '../../components/meu/Hero';
+// import KeyFindings from '../../components/meu/KeyFindings';
+// import Recommendations from '../../components/meu/Recommendations';
+// import Navbar from '../../components/Navbar';
 
-const Index = () => {
-  return (
-    <>
-      <Navbar />
-      <div className="min-h-screen bg-[#1a1a1a] font-['Inter'] text-white">
-        <Hero />
-        <KeyFindings />
-        <Recommendations />
-      </div>
-    </>
-  );
-};
+// const Index = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="min-h-screen bg-[#1a1a1a] font-['Inter'] text-white">
+//         <Hero />
+//         <KeyFindings />
+//         <Recommendations />
+//       </div>
+//     </>
+//   );
+// };
 
-export default Index;
+// export default Index;
