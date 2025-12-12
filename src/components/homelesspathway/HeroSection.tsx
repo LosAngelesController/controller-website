ef9f8209bae8062903f4163d2bef474b5f77880e
Vartan Arzumanyan <bigballer131@gmail.com>
2025-12-12 08:00:55 -0800

    uuu

--- src/components/homelesspathway/HeroSection.tsx
+++ src/components/homelesspathway/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { Download } from "lucide-react";
 import { Button } from "@/components/homelesspathway/ui/button";
 import coverImage from "@/components/homelesspathway/assets/pathwayscover.jpg";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
@@ -47,11 +48,17 @@ const HeroSection = () => {
           </div>
 
           {/* Image */}
-          <img
+          <Image
+            src={coverImage}
+            alt="Homelessness Audit: Pathways to Permanent Housing cover"
+            className="w-full h-auto rounded-lg shadow-2xl"
+            priority
+          />
+          {/* <img
             src={coverImage.src}
             alt="Homelessness Audit: Pathways to Permanent Housing cover"
             className="w-full h-auto rounded-lg shadow-2xl"
-          />
+          /> */}
         </div>
       </div>
     </section>
