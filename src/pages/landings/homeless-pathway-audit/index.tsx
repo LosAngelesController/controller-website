7f71a2ed06425594270cfe14d9e3bdfcd9145962
Isaac Lee <32625683+isaacylee@users.noreply.github.com>
2026-01-29 09:47:00 -0800

    Add meta description for SEO

--- src/pages/landings/homeless-pathway-audit/index.tsx
+++ src/pages/landings/homeless-pathway-audit/index.tsx
@@ -3,10 +3,17 @@ import KeyFindingsSection from '@/components/homelessPathway/KeyFindingsSection'
 import RecommendationsSection from '@/components/homelessPathway/RecommendationsSection';
 import SuccessFactorsSection from '@/components/homelessPathway/SuccessFactorsSection';
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 
 const Index = () => {
   return (
     <main className='bg-background min-h-screen'>
+      <Head>
+        <meta
+          name='description'
+          content='LA Controller’s Homeless Pathway Audit summarizes findings, data, and recommendations to improve homelessness response and outcomes in Los Angeles.'
+        />
+      </Head>
       <Navbar />
       <HeroSection />
       <KeyFindingsSection />
